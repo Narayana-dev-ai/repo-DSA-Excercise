d358ecf450c1addb0e827e76853e75f6a74e1c25
Narayana <lakkireddy.narayana-reddy@airbus.com>
2024-10-10 23:28:17 +0530

    inserting node into list done

--- Linear_DSA/DataStructures/LinkedListOperations.py
+++ Linear_DSA/DataStructures/LinkedListOperations.py
@@ -36,6 +36,24 @@ def deleteSpecificNode(head, deleteNode):
     
     return head
         
+def insertAtSpecificPosition(head, insertNode, pos):
+    if pos == 1:
+        insertNode.next = head
+        return insertNode
+    
+    current_node = head
+    for _ in range(pos-2):
+        if current_node.next is None:
+            break
+        current_node = current_node.next
+   
+    if current_node is None:
+        current_node.next = newNode
+    else:
+        newNode.next = current_node.next
+        current_node.next = newNode
+    
+    return head
     
 node1 = Node(7)
 node2 = Node(11)
@@ -48,7 +66,13 @@ node2.next = node3
 node3.next = node4
 node4.next = node5
 
-print("Before deletion:")
+print("Before Any Operation:")
+traverseList(node1)
+
+# Insert newNode
+newNode = Node(69)
+node1 = insertAtSpecificPosition(node1, newNode, 7)
+print("\nAfter Insertion:")
 traverseList(node1)
 
 # Delete node3
